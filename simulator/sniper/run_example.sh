5a8179a95caac20e3959c6c21e730ddf6bafbc40
Pravesh Jamgade <praveshjamgade@gmail.com>
2025-05-02 15:44:51 +0530

    update run_exmaple script

--- simulator/sniper/run_example.sh
+++ simulator/sniper/run_example.sh
@@ -4,11 +4,32 @@
 # This script runs the Sniper simulator with a specified configuration file and workload.
 # Usage: ./run_example.sh 
 
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -a parameterA -b parameterB -c parameterC"
+   echo -e "\t-a Application"
+   echo -e "\t-b Input"
+   echo -e "\t-c Description of what is parameterC"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "a:b:c:" opt
+do
+   case "$opt" in
+      a ) parameterA="$OPTARG" ;;
+      b ) parameterB="$OPTARG" ;;
+      c ) parameterC="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+
 CONFIG_FILE=./config/virtuoso_configs/virtuoso_baseline.cfg
 
 WORKLOAD=ls
 
-./run-sniper -c $CONFIG_FILE -d ./example_output --genstats -- $WORKLOAD
+./run-sniper -c $CONFIG_FILE -d ./example_output --genstats -- $parameterA $parameterB
 
 
 #Check if the command was successful by looking for sim.stats in the output directory
